03e87b141365fc5e413b829d7c40ef059be88c4f
luiseok <iam@luiseok.com>
2022-04-12 04:55:59 +0900

    refactor: change let to const

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import cheerio from 'cheerio';
 import {FCEVInterface, FCEVReceiptInterface} from "./types/FCEV";
 
 const parseFCEVCount = (textString:string): FCEVReceiptInterface => {
-    let parsedList = textString
+    const parsedList = textString
         .split(" ")
         .map(text => text.trim())
         .map(parsed=> parseInt(parsed.replace(/\(|\)/g, ""), 10) || 0)
-    let result:FCEVReceiptInterface;
-    result = {
+
+    const result:FCEVReceiptInterface = {
         total: parsedList.length < 3 ? parsedList.reduce((acc, cur) => {return acc += cur }, 0) : parsedList[0],
         priority: parsedList[parsedList.length - 2],
         general: parsedList[parsedList.length - 1]
@@ -22,17 +22,16 @@ const requestFCEVStatus = async () => {
     const {data} = await axios.get(subsidyStatus.FCEV);
     const $ = cheerio.load(data);
 
-    let listOfStatus: FCEVInterface[] = [];
+    const listOfStatus: FCEVInterface[] = [];
 
     const tableBody = $(".table_02_2_1 tbody > tr");
     tableBody.each((index, element) => {
-        let row:FCEVInterface;
         const el = $(element), prevEl = $(tableBody[index - 1])
 
         const category = el.find("th:last-of-type").text();
 
         const hasMultiCategory = prevEl.children().length > 0 && el.children("th").length <= 1;
-        row = {
+        const row: FCEVInterface= {
             province: hasMultiCategory ? listOfStatus[index - 1].province : el.find("th:first-of-type").text(),
             city: hasMultiCategory ? listOfStatus[index - 1].city : el.find("th:nth-of-type(2)").text(),
             category,
